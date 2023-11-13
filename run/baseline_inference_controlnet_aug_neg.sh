b96340ec79161e5ef4072d3b4870cfa9da9a798d
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-14 00:15:02 +0700

    update

--- run/baseline_inference_controlnet_aug_neg.sh
+++ run/baseline_inference_controlnet_aug_neg.sh
@@ -3,5 +3,5 @@ config='configs/baseline_controlnet_aug.yaml'
 accelerate launch --mixed_precision="fp16" test_controlnet.py \
                   --config $config \
                   --model_dir "checkpoints/baseline_controlnet_aug" \
-                  --output_dir "inference/baseline_controlnet_aug_neg" \ 
-                  --negative_prompt "blurry, pixelated, noisy, distorted, low-resolution images"
+                  --negative_prompt "blurry, pixelated, noisy, distorted, low-resolution images" \
+                  --output_dir "inference/baseline_controlnet_aug_neg"  
