f3abe7e25fa49949f9189420a9f8b0cc291d04c9
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-15 23:05:43 +0700

    uppdate

--- run/baseline_aug_vis.sh
+++ run/baseline_aug_vis.sh
@@ -2,5 +2,5 @@ config='configs/baseline_aug_vis.yaml'
 
 accelerate launch --mixed_precision="fp16" train_vis.py \
                   --config $config \
-                  --output_dir "checkpoints/baseline_aug_vis"\
-                  --resume_from_checkpoint "latest"
+                  --output_dir "checkpoints/baseline_aug_vis"
+#                  --resume_from_checkpoint "latest"
