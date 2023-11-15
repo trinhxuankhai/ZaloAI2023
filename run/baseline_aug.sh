4d7c2062d38632f5aff4362ccbc91616f5b4b475
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-16 00:19:52 +0700

    update

--- run/baseline_aug.sh
+++ run/baseline_aug.sh
@@ -1,6 +1,6 @@
 config='configs/baseline_aug.yaml'
 
-accelerate launch --mixed_precision="fp16" train.py \
+accelerate launch --mixed_precision="fp16" -multi_gpu --num_processes=2 train.py \
                   --config $config \
                   --output_dir "checkpoints/baseline_aug_v2"\
                   --resume_from_checkpoint "latest"
