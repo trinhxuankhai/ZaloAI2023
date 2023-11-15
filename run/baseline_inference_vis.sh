09e78c2ae11c3ff4e3116bbb6a4e1e2cce75104b
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-15 19:32:06 +0700

    update

--- run/baseline_inference_vis.sh
+++ run/baseline_inference_vis.sh
@@ -4,4 +4,4 @@ accelerate launch --mixed_precision="fp16" test_vis.py \
                   --config $config \
                   --output_dir inference/baseline_aug_vis \
                   --resume_from_checkpoint "latest" \
-                  --checkpoint_dir "checkpoints/baseline_aug_vis" \ 
+                  --checkpoint_dir "checkpoints/baseline_aug_vis" 
