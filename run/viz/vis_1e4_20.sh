46d1c858f2e21adf07667f698586311efdeb0c71
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-20 17:09:15 +0700

    update

--- run/viz/vis_1e4_20.sh
+++ run/viz/vis_1e4_20.sh
@@ -1,5 +1,5 @@
 config='configs/baseline_aug_vis_v3.yaml'
 
-accelerate launch --mixed_precision="fp16" train_vis.py \
+accelerate launch --gpu_ids 1 --mixed_precision="fp16" train_vis.py \
                   --config $config \
                   --output_dir "checkpoints/vis/vis_1e4_20"
