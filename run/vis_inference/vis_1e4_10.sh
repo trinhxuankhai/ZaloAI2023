512cfc843544bed8dea6a92d451926d0841b7351
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-20 19:24:58 +0700

    update

--- run/vis_inference/vis_1e4_10.sh
+++ run/vis_inference/vis_1e4_10.sh
@@ -1,5 +1,5 @@
 config='configs/vis_1e4_10.yaml'
 
-python3 test_viz.py --output_dir "inference/vis_finetune/vis_1e4_10" \
+python3 test_viz.py --mixed_precision="fp16" --output_dir "inference/vis_finetune/vis_1e4_10" \
                     --config $config \
                     --resume_from_checkpoint "checkpoints/vis/vis_1e4_10/final-checkpoint"
