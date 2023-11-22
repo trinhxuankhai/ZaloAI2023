1502defe26d4f225d015ccffa16e6e0778db1289
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-22 18:13:26 +0700

    update

--- run/vis_train.sh
+++ run/vis_train.sh
@@ -1,5 +1,5 @@
 config='configs/vis_1e6_10_crawl.yaml'
 
 python3 test_viz_v2.py --mixed_precision="fp16" --output_dir "inference/vis_train" \
-                       -config $config \
+                       --config $config \
                        --resume_from_checkpoint "checkpoints/vis/vis_1e6_10_crawl/final-checkpoint"
