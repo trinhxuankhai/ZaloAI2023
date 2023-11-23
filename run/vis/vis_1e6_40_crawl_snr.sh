2da1380801093dbe8b3113e7d54f0131d4be3ee0
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-23 09:55:17 +0700

    update

--- run/vis/vis_1e6_40_crawl_snr.sh
+++ run/vis/vis_1e6_40_crawl_snr.sh
@@ -1,6 +1,6 @@
 config='configs/vis_1e6_40_crawl.yaml'
 
-accelerate launch --gpu_ids 1 --mixed_precision="fp16" train_vis.py \
+accelerate launch --gpu_ids 0 --mixed_precision="fp16" train_vis.py \
                   --config $config \
                   --output_dir "checkpoints/vis/vis_1e6_40_crawl_snr" \
                   --snr_gamma 5.0
