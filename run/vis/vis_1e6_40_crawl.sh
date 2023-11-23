4aa4cd924e1b1bf7232752f2f054c5bdca5c1aad
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-23 09:50:41 +0700

    update

--- run/vis/vis_1e6_40_crawl.sh
+++ run/vis/vis_1e6_40_crawl.sh
@@ -1,5 +1,5 @@
-config='configs/vis_1e6_100_crawl.yaml'
+config='configs/vis_1e6_40_crawl.yaml'
 
 accelerate launch --gpu_ids 1 --mixed_precision="fp16" train_vis.py \
                   --config $config \
-                  --output_dir "checkpoints/vis/vis_1e6_100_crawl"
+                  --output_dir "checkpoints/vis/vis_1e6_40_crawl"
