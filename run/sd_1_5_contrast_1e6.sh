32042fe6eda02349a8866cbb755ec07bd573cd9a
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-27 17:14:25 +0700

    update

--- run/sd_1_5_contrast_1e6.sh
+++ run/sd_1_5_contrast_1e6.sh
@@ -3,5 +3,4 @@ config='configs/sd_1_5_10.yaml'
 accelerate launch --gpu_ids 0 --mixed_precision="fp16" train.py \
                   --config $config \
                   --prediction_type="v_prediction" \
-                  --vae \
                   --output_dir "checkpoints/sd_1_5_10"
