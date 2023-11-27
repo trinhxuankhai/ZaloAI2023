e0d5d5f3dfb9629512d81a941c55ca0f084cfdb3
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-27 17:50:49 +0700

    update

--- run/sd_2_1_contrast_30.sh
+++ run/sd_2_1_contrast_30.sh
@@ -1,6 +1,6 @@
-config='configs/sd_2_1_10.yaml'
+config='configs/sd_2_1_30.yaml'
 
 accelerate launch --gpu_ids 0 --mixed_precision="fp16" train.py \
                   --config $config \
                   --prediction_type="v_prediction" \
-                  --output_dir "checkpoints/sd_2_1_10"
+                  --output_dir "checkpoints/sd_2_1_30"
