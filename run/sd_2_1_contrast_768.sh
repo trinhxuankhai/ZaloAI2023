d3890936f6182a312747fb42454c20e4e44ca69a
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-28 00:01:27 +0700

    update

--- run/sd_2_1_contrast_768.sh
+++ run/sd_2_1_contrast_768.sh
@@ -1,6 +1,6 @@
 config='configs/sd_2_1_50_768.yaml'
 
-accelerate launch --mixed_precision="fp16" train_v2.py \
+accelerate launch --mixed_precision="fp16" train.py \
                   --config $config \
                   --prediction_type "v_prediction" \
                   --checkpointing_steps 228 \
