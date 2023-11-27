7cd183a6b9caf99c8fc064ee9f01d16bf6751b9a
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-27 23:20:19 +0700

    update

--- run/sd_2_1.sh
+++ run/sd_2_1.sh
@@ -4,4 +4,4 @@ accelerate launch --gpu_ids 0 --mixed_precision="fp16" train.py \
                   --config $config \
                   --checkpointing_steps 228 \
                   --checkpoints_total_limit 10 \
-                  --output_dir "checkpoints/sd_2_1_50_cap/checkpoint-4332"
+                  --output_dir "checkpoints/sd_2_1_50_cap"
