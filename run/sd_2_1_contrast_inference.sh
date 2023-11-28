3c366a40cc22444c3d3f4ffd93635f0501b801dd
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-28 08:01:52 +0700

    update

--- run/sd_2_1_contrast_inference.sh
+++ run/sd_2_1_contrast_inference.sh
@@ -1,6 +1,6 @@
 config='configs/sd_2_1_50.yaml'
 
-python3 test.py --mixed_precision="fp16" --output_dir "inference/sd_2_1_cap_contrast" \
+python3 test.py --mixed_precision="fp16" --output_dir "inference/sd_2_1_cap_contrast_8500" \
                     --config $config \
                     --prediction_type="v_prediction" \
                     --resume_from_checkpoint "checkpoints/sd_2_1_50/checkpoint-8500"
--- run/sd_2_1_inference.sh
+++ run/sd_2_1_inference.sh
@@ -2,4 +2,4 @@ config='configs/sd_2_1_50.yaml'
 
 python3 test.py --mixed_precision="fp16" --output_dir "inference/sd_2_1_cap_mid" \
                     --config $config \
-                    --resume_from_checkpoint "checkpoints/sd_2_1_50_cap/checkpoint-4332"
+                    --resume_from_checkpoint "checkpoints/sd_2_1_50_cap"
