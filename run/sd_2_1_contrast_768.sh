9b591b8d79e1f52b61429be7ed78a98d8c54a782
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-28 08:13:36 +0700

    update

--- run/sd_2_1_contrast_768.sh
+++ run/sd_2_1_contrast_768.sh
@@ -1,6 +1,6 @@
 config='configs/sd_2_1_50_768.yaml'
 
-accelerate launch --multi_gpu --gpu_ids "[0,1]" --mixed_precision="fp16" train.py \
+accelerate launch --multi_gpu --gpu_ids "0,1" --mixed_precision="fp16" train.py \
                   --config $config \
                   --prediction_type "v_prediction" \
                   --checkpointing_steps 228 \
