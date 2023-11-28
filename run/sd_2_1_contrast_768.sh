65a0e3e0e1807d7f6897164b4f5dfcd79ad7821a
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-28 08:43:19 +0700

    update

--- run/sd_2_1_contrast_768.sh
+++ run/sd_2_1_contrast_768.sh
@@ -5,5 +5,5 @@ accelerate launch --use_deepspeed --gpu_ids "0,1" --mixed_precision="fp16" train
                   --prediction_type "v_prediction" \
                   --checkpointing_steps 228 \
                   --checkpoints_total_limit 10 \
-                  --resume_from_checkpoint "latest" \
+                #   --resume_from_checkpoint "latest" \
                   --output_dir "checkpoints/sd_2_1_50_768"
