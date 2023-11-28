907438a81884a7dbf97c11d1d8bed6ffe2631e4a
trinhxuankhai <trinhxuankhai2310@gmail.com>
2023-11-28 08:03:09 +0700

    update

--- run/sd_2_1_inference.sh
+++ run/sd_2_1_inference.sh
@@ -1,5 +1,5 @@
 config='configs/sd_2_1_50.yaml'
 
-python3 test.py --mixed_precision="fp16" --output_dir "inference/sd_2_1_cap_mid" \
+python3 test.py --mixed_precision="fp16" --output_dir "inference/sd_2_1" \
                     --config $config \
-                    --resume_from_checkpoint "checkpoints/sd_2_1_50_cap"
+                    --resume_from_checkpoint "checkpoints/sd_2_1_50_cap/final-checkpoint"
